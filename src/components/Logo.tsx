431ce7ee7d99dcd0ddfea33e6bdbd1313c86d716
DIEGOHORVATTI <d.horvattid@gmail.com>
2025-08-25 23:45:59 -0300

    feat: update Footer and Logo components with new styles and fonts
    - Added spacing to the Footer component for better layout.
    - Modified Logo component to include text alongside the image and adjusted image size.
    - Imported Montserrat font from Google Fonts in index.css.
    - Extended Tailwind CSS configuration to include Montserrat font family.

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,8 +2,13 @@ import { Link } from 'react-router'
 
 export default function Logo() {
   return (
-    <Link to="/" className="flex items-center space-x-2">
-      <img src="/logo.svg" alt="Logo" className="w-20" />
+    <Link to="/" className="flex items-center space-x-3">
+      <img src="/logo.svg" alt="Logo" className="w-12 h-auto" />
+
+      <div className="flex flex-col font-montserrat">
+        <span className="text-sm tracking-wider">HORVATTI</span>
+        <span className="text-xl font-bold tracking-wide">CHAMP</span>
+      </div>
     </Link>
   )
 }
